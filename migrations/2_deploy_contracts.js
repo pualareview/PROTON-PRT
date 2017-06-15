1b716f62b83e8aecf339ff2a43cdbb8cf760760d
Jaynti Kanani <jdkanani@gmail.com>
2017-06-16 00:30:29 +0530

    deploy with arguments

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,7 +1,15 @@
 var ProtonToken = artifacts.require("./ProtonToken.sol");
 var MultiSigWallet = artifacts.require("./MultiSigWallet.sol");
 
-module.exports = function(deployer) {
-  // deployer.deploy(MultiSigWallet);
-  // deployer.deploy(ProtonToken);
+module.exports = function(deployer, network) {
+  let accounts = web3.eth.accounts;
+
+  // deploy multisig wallet
+  deployer.deploy(MultiSigWallet, accounts.slice(0, 2), 2).then(()=>{
+      const startBlock = web3.eth.blockNumber + 10;
+      const endBlock = startBlock + (4 * 5); // 1 day = 5760 blocks, 1 minute = 4 blocks
+
+      // deploy proton token contract
+      deployer.deploy(ProtonToken, MultiSigWallet.address, accounts[0], startBlock, endBlock);
+  });
 };
